e5325f21e9a5c9fd71bab4721b10175637292a4e
siddhigate12 <siddhi@sharefable.com>
2024-01-12 18:02:46 +0530

    header

--- .components/header/Header.jsx
+++ .components/header/Header.jsx
@@ -81,7 +81,6 @@ const Header = (props) => {
                         title={link.title}
                         subTitle={link.subtitle}
                         subLinks={link.sublinks}
-                        banner={link.banner}
                       />
                     </li>
                   )
@@ -219,6 +218,7 @@ function Menu(props) {
   const [showMenu, setShowMenu] = useState(false)
   return (
     <>
+    <div style={{position: "relative"}}>
       <span
         className='header-link'
         onMouseEnter={() => setShowMenu(true)}
@@ -244,26 +244,25 @@ function Menu(props) {
         onMouseLeave={() => setShowMenu(false)}
         className={`menu ${showMenu ? 'menu-visible' : 'menu-hidden'}`}
         style={{
-          width: '100%',
+          width: 'fit-content',
+          minWidth: "360px",
           maxWidth: '1500px',
           height: 'fit-content',
           backgroundColor: 'transparent',
           position: 'absolute',
-          top: '100px',
+          top: '40px',
           left: '50%',
           zIndex: -1,
         }}
       >
         <div
           style={{
-            margin: '1rem 1rem 0 1rem',
-            width: 'calc(100% - 4rem)',
+            margin: '0.5rem',
             height: '100%',
             backgroundColor: '#fff',
             borderRadius: '0.6rem',
             display: 'flex',
             padding: '1rem',
-            gap: '1rem',
             alignItems: 'stretch'
           }}
         >
@@ -273,27 +272,12 @@ function Menu(props) {
               padding: '1rem 1.2rem'
             }}
           >
-            <div
-              style={{
-                borderBottom: '1px solid black',
-              }}
-            >
-              <p
-                style={{
-                  lineHeight: '1',
-                  margin: '0',
-                  paddingBottom: '0.4rem'
-                }}
-              >
-                {props.subTitle}
-              </p>
-            </div>
             <ul
               style={{
                 listStyle: 'none',
                 padding: 0,
                 display: 'grid',
-                gridTemplateColumns: '1fr 1fr',
+                gridTemplateColumns: '1fr',
                 gap: '0.4rem',
                 marginTop: '1rem'
               }}
@@ -309,18 +293,16 @@ function Menu(props) {
                     <a
                       href={link.url}
                       style={{
-                        color: 'inherit',
+                        color: '#000',
                         fontSize: '0.9rem',
                         display: 'flex',
                         gap: '0.6rem',
                         alignItems: 'center',
                         padding: '0.5rem',
-                        width: 'calc(100% - 1rem)',
                         borderRadius: '8px'
                       }}
                       className='menu-links'
                     >
-                      <img src={link.logo} alt="logo" />
                       <div
                       >
                         <p
@@ -350,78 +332,9 @@ function Menu(props) {
               })}
             </ul>
           </div>
-          <div
-            style={{
-              flex: '0.4',
-              background: 'linear-gradient(90deg,#fedf64,#ff7450,#7567ff)',
-              borderRadius: '6px',
-              padding: '1px',
-            }}
-          >
-            <div
-              style={{
-                backgroundColor: '#fff',
-                borderRadius: '6px',
-                padding: '1rem 2rem',
-                height: 'calc(100% - 2rem)'
-              }}
-            >
-
-              <div
-                style={{
-                  borderBottom: '1px solid black',
-                }}
-              >
-                <p
-                  style={{
-                    lineHeight: '1',
-                    margin: '0',
-                    fontSize: '0.9rem',
-                    paddingBottom: '0.4rem'
-                  }}
-                >
-                  {props.banner.title}
-                </p>
-              </div>
-              <div
-                style={{
-                  marginTop: '1rem'
-                }}
-              >
-                <p
-                  style={{
-                    lineHeight: '1',
-                    margin: '0',
-                    fontWeight: '600',
-                    fontSize: '0.9rem'
-                  }}
-                >
-                  {props.banner.heading}
-                </p>
-                <p
-                  style={{
-                    lineHeight: '1',
-                    margin: '0',
-                    marginTop: '0.5rem',
-                    fontWeight: '300',
-                    fontSize: '0.9rem'
-                  }}
-                >
-                  {props.banner.description}
-                </p>
-                <div
-                  style={{
-                    marginTop: '1rem'
-                  }}
-                >
-                  <a href={props.banner.cta.url} className='banner-title'>
-                    {props.banner.cta.title}
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
+      </div>
       </div>
     </>
   )
--- config.js
+++ config.js
@@ -16,86 +16,39 @@ module.exports = {
       navLinks: {
         alignment: 'right',
         links: [
-          { title: 'Product', url: 'https://sharefable.com/products' },
+          { title: 'Product', url: 'https://www.theden.ai/chatden' },
           {
             title: 'Solutions',
             subtitle: 'The ultimate demo platform for all your GTM needs',
             sublinks: [
               {
-                title: 'Marketing',
-                subtitle: 'Increase conversions from your existing funnel',
-                logo: 'https://sharefable.com/logo/marketing-logo.svg',
+                title: 'Customer Support',
                 url: 'https://sharefable.com/solutions/marketing'
               },
               {
-                title: 'Sales',
-                subtitle: 'Create deal-winning demos every single time',
-                logo: 'https://sharefable.com/logo/sales-logo.svg',
+                title: 'Lead Generation',
                 url: 'https://sharefable.com/solutions/sales'
               },
               {
-                title: 'Presales',
-                subtitle: 'Scale demo environments & personalize in minutes',
-                logo: 'https://sharefable.com/logo/presales-logo.svg',
+                title: 'e-Commerce',
                 url: 'https://sharefable.com/solutions/presales'
               },
               {
-                title: 'Partnerships',
-                subtitle: 'Enable partners and skyrocket channel sales',
-                logo: 'https://sharefable.com/logo/partnerships-logo.svg',
+                title: 'Internal Wiki',
                 url: 'https://sharefable.com/solutions/partnerships'
               },
-            ],
-            banner: {
-              title: 'You\'d not want to miss this!',
-              heading: 'Create personalized demos to close more deals',
-              description: 'Our CEO was feature in Tekpon\'s podcast where he shared valuable insights on... well... how to close more deals!',
-              cta: {
-                title: 'Give it a listen',
-                url: 'https://tekpon.com/podcasts/create-personalized-product-demos-to-close-more-deals/'
-              }
-            }
+              {
+                title: 'HR Assistant',
+                url: 'https://sharefable.com/solutions/partnerships'
+              },
+              {
+                title: 'Academic Research',
+                url: 'https://sharefable.com/solutions/partnerships'
+              },
+            ]
           },
-          {
-            title: 'Resources',
-            subtitle: 'Made with love, from Fable\'s stable',
-            sublinks: [
-              {
-                title: 'Fable Champs',
-                subtitle: 'How hotshot SaaS companies achieved hypergrowth',
-                logo: 'https://sharefable.com/logo/fable-champs-logo.svg',
-                url: 'https://sharefable.com/growth-stories'
-              },
-              {
-                title: 'Fable Untold',
-                subtitle: 'The small corner of the internet for inspiring growth stories',
-                logo: 'https://sharefable.com/logo/fables-untold-logo.svg',
-                url: 'https://sharefable.com/podcast/mastering-b2b-demand-generation'
-              },
-              {
-                title: 'E-books',
-                subtitle: 'Handy resources that all GTM teams will love',
-                logo: 'https://sharefable.com/logo/e-books-logo.svg',
-                url: 'https://sharefable.com/ebooks'
-              },
-              {
-                title: 'Fable blog',
-                subtitle: 'Handy resources that all GTM teams will love',
-                logo: 'https://sharefable.com/logo/blog-logo.svg',
-                url: 'https://sharefable.com/ebooks'
-              }
-            ],
-            banner: {
-              title: 'You\'d not want to miss this!',
-              heading: 'Create personalized demos to close more deals',
-              description: 'Our CEO was feature in Tekpon\'s podcast where he shared valuable insights on... well... how to close more deals!',
-              cta: {
-                title: 'Give it a listen',
-                url: 'https://tekpon.com/podcasts/create-personalized-product-demos-to-close-more-deals/'
-              }
-            }
-          },
-          { title: 'Pricing', url: 'https://sharefable.com/pricing' },
+          { title: 'Pricing', url: 'https://www.theden.ai/pricing' },
+          { title: 'Blog', url: 'https://www.theden.ai/blog' },
         ]
       },
       cta: {
